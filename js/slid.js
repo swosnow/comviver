b78169e8af3985a57bf921e98d391dd64c38da2e
Isaque Anderson <isaqanderson23@gmail.com>
2024-07-28 00:27:41 -0300

    section update

---
+++ js/slid.js
@@ -0,0 +1,54 @@
+let nextBtn = document.querySelector('.next')
+let prevBtn = document.querySelector('.prev')
+
+let slider = document.querySelector('.slider')
+let sliderList = slider.querySelector('.slider .list')
+let thumbnail = document.querySelector('.slider .thumbnail')
+let thumbnailItems = thumbnail.querySelectorAll('.item2')
+
+const navbar = document.querySelector("nav");
+const menu = document.querySelector(".meNU");
+const menuBtn = document.querySelector(".menu-btn")
+
+menuBtn.addEventListener("click", () => {
+    navbar.classList.toggle('nav-background');
+    menu.classList.toggle('nav-toggle');
+});
+
+thumbnail.appendChild(thumbnailItems[0])
+
+// Function for next button 
+nextBtn.onclick = function() {
+    moveSlider('next')
+}
+
+
+// Function for prev button 
+prevBtn.onclick = function() {
+    moveSlider('prev')
+}
+
+
+function moveSlider(direction) {
+    let sliderItems = sliderList.querySelectorAll('.item2')
+    let thumbnailItems = document.querySelectorAll('.thumbnail .item2')
+    
+    if(direction === 'next'){
+        sliderList.appendChild(sliderItems[0])
+        thumbnail.appendChild(thumbnailItems[0])
+        slider.classList.add('next')
+    } else {
+        sliderList.prepend(sliderItems[sliderItems.length - 1])
+        thumbnail.prepend(thumbnailItems[thumbnailItems.length - 1])
+        slider.classList.add('prev')
+    }
+
+
+    slider.addEventListener('animationend', function() {
+        if(direction === 'next'){
+            slider.classList.remove('next')
+        } else {
+            slider.classList.remove('prev')
+        }
+    }, {once: true}) // Remove the event listener after it's triggered once
+}
