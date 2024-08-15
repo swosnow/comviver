d4c4c4453deb9764f2446cca38b019cf44b61967
Isaque Anderson <isaqanderson23@gmail.com>
2024-08-15 11:08:22 -0300

    page update

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,11 @@ sr.reveal('.menu-space', {duration:900});
 function openWhatsApp() {
   window.open('https://wa.me/5521997271258', '_blank');
 }
+const botao = document.querySelector('#botao')
 
+botao.addEventListener('click', () => {
+    window.scroll({top: window.innerHeight, behavior: "smooth"})
+   
+})
 
 
